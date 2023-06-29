b0e1cafd03c2a2159dedacde37b28812c9f1e3ee
Sebastian Schepis <sschepis@gmail.com>
2023-06-29 14:35:16 -0700

    initial commit

--- test/ERC735.ts
+++ test/ERC735.ts
@@ -1,34 +1,59 @@
 import { expect } from 'chai';
-import { ethers, deployments, getUnnamedAccounts, getNamedAccounts } from 'hardhat';
+import { ethers, deployments, getUnnamedAccounts } from 'hardhat';
 import { IERC735 } from '../typechain-types';
 import { setupUser, setupUsers } from './utils';
 
 const setup = deployments.createFixture(async () => {
 	await deployments.fixture('ERC735');
-	const { simpleERC20Beneficiary } = await getNamedAccounts();
 	const contracts = {
-		SimpleERC20: await ethers.getContract<IERC735>('ERC735'),
+		ERC735: await ethers.getContract<IERC735>('ERC735'),
 	};
 	const users = await setupUsers(await getUnnamedAccounts(), contracts);
 	return {
 		...contracts,
 		users,
-		simpleERC20Beneficiary: await setupUser(simpleERC20Beneficiary, contracts),
 	};
 });
 
-describe('SimpleERC20', function () {
-	it('transfer fails', async function () {
-		const { users } = await setup();
-		await expect(users[0].SimpleERC20.transfer(users[1].address, 1)).to.be.revertedWith('NOT_ENOUGH_TOKENS');
-	});
+describe('ERC735', function () {
+	it('addClaim, getClaim, changeClaim, removeClaim', async function () {
+		const { users, ERC735 } = await setup();
+		const topic = 1;
+		const scheme = 1;
+		const issuer = users[0].address;
+		const signature = "0x123";
+		const data = "0x123";
+		const uri = "uri";
 
-	it('transfer succeed', async function () {
-		const { users, simpleERC20Beneficiary, SimpleERC20 } = await setup();
-		await simpleERC20Beneficiary.SimpleERC20.transfer(users[1].address, 1);
+		// Add claim
+		const claimRequestId = await ERC735.connect(users[0].ERC735 as any).addClaim(topic, scheme, issuer, signature, data, uri);
 
-		await expect(simpleERC20Beneficiary.SimpleERC20.transfer(users[1].address, 1))
-			.to.emit(SimpleERC20, 'Transfer')
-			.withArgs(simpleERC20Beneficiary.address, users[1].address, 1);
+		await expect(ERC735.addClaim(topic, scheme, issuer, signature, data, uri))
+			.to.emit(ERC735, 'ClaimRequested')
+			.withArgs(claimRequestId, topic, scheme, issuer, signature, data, uri);
+
+		// Get claim
+		const claim = await ERC735.getClaim(claimRequestId as any);
+		expect(claim).to.include({
+			topic: topic,
+			scheme: scheme,
+			issuer: issuer,
+			signature: signature,
+			data: data,
+			uri: uri
+		});
+
+		// Change claim
+		const newSignature = "0x456";
+		const newData = "0x456";
+		const newUri = "uri2";
+		await expect(ERC735.connect(users[0].ERC735 as any).changeClaim(claimRequestId as any, topic, scheme, issuer, newSignature, newData, newUri))
+			.to.emit(ERC735, 'ClaimChanged')
+			.withArgs(claimRequestId, topic, scheme, issuer, newSignature, newData, newUri);
+
+		// Remove claim
+		await expect(ERC735.connect(users[0].ERC735 as any).removeClaim(claimRequestId as any))
+			.to.emit(ERC735, 'ClaimRemoved')
+			.withArgs(claimRequestId, topic, scheme, issuer, signature, data, uri);
 	});
 });
